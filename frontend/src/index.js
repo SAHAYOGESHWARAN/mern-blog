d325536002830de764d57fb1498d364a29419856
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-02 10:02:37 +0530

    mern

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';  // Make sure App.js is in the same directory as index.js
+ // Optional: include if you have a CSS file
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );

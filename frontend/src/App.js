cb331f9adc688112cf52a95005d4c4f2016000e4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-02 10:18:19 +0530

    mern

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,11 @@ import Login from './components/Login';
 import Home from './components/Home';
 import EditPost from './components/EditPost';
 import Post from './components/Post';
-import CreatePost from './components/CreatePost';
 
 const App = () => {
   const handleLogout = () => {
     localStorage.removeItem('token');
-    window.location.reload();
+    window.location.reload(); 
   };
 
   return (
@@ -20,14 +19,12 @@ const App = () => {
           <Link to="/">Home</Link>
           <Link to="/register">Register</Link>
           <Link to="/login">Login</Link>
-          <Link to="/create">Create Post</Link>
           <button onClick={handleLogout}>Logout</button>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/create" element={<CreatePost />} />
           <Route path="/post/edit/:id" element={<EditPost />} />
           <Route path="/post/:id" element={<Post />} />
         </Routes>
